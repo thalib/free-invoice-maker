fd887796533203256dd6da1c16603bb5b42258e0
Mohamed Thalib <hmthalib@gmail.com>
2023-12-23 15:43:00 +0530

    support for loading company details from file added

---
+++ v2.0/company.js
@@ -0,0 +1,12 @@
+
+function update_company_settings() {
+  /**
+   * Edit below values to change company name and address
+   */
+  console.log("update_company_settings");
+  document.getElementById('company_name').value = "DEVNODES.IN";
+  document.getElementById('company_addr').value = `India. PIN - 654321.
+  TEL: 987654321, Email: support@example.in
+  Website: www.devnodes.in 
+`;
+}
